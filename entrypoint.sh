c404a49eec8f27b0c38e4cbeca3faa5a388e9c26
Gautam <baghel.g@husky.neu.edu>
2019-10-02 14:55:05 -0400

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bash <(curl -s https://detect.synopsys.com/detect.sh) \
---blackduck.api.token="$BLACKDUCK_API_TOKEN" \
---blackduck.url="$BLACKDUCK_URL" \
-"$*"
+
+export JAVA_HOME="/usr/local/openjdk-8"
+
+bash <(curl -s https://detect.synopsys.com/detect.sh) "$*"
