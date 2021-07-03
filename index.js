97af6cf067b531fea396831ab89439e40200abc2
krishna-chaitanya-by <65754312+krishna-chaitanya-by@users.noreply.github.com>
2021-07-04 03:12:59 +0530

    Upgraded detect to 7.x

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ if (IS_WINDOWS) {
     returnCode = shell.exec(`powershell "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; irm https://detect.synopsys.com/detect.ps1?$(Get-Random) | iex; detect ${detectArgs}"`).code;
   } else {
     // On everything else do bash
-    shell.exec("wget https://detect.synopsys.com/detect.sh")
-    shell.exec("chmod +x detect.sh")
-    returnCode = shell.exec(`./detect.sh ${detectArgs}`).code;
+    shell.exec("wget https://detect.synopsys.com/detect7.sh")
+    shell.exec("chmod +x detect7.sh")
+    returnCode = shell.exec(`./detect7.sh ${detectArgs}`).code;
 }
 
 if (returnCode == 3) {
